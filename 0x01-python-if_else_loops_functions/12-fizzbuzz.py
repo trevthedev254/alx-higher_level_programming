9c2f5480aa15200aaa12d2fedb093c1576effbaf
trevthedev254 <trevetech@gmail.com>
2022-06-28 17:08:05 +0000

    power

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if (i / 3) == 0 and (i / 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             print("FizzBuzz ")
-        elif (i / 3) == 0 and (i / 5) != 0:
+        elif (i % 3) == 0 and (i % 5) != 0:
             print("Fizz ")
-        elif (i / 5) == 0 and (i / 3) != 0:
+        elif (i % 5) == 0 and (i % 3) != 0:
             print("Buzz ")
