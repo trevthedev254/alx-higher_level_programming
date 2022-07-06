9ad0fd3b0b3ce5f61261d308449fff59ca408fd4
trevthedev254 <trevetech@gmail.com>
2022-07-06 16:20:10 +0000

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for elm in my_list:
-        if elm == search:
-            return elm
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            my_list[i] = replace
+            return my_list
         else:
-            return replace
+            return my_list           
