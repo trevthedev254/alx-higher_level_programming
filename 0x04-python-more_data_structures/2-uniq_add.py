439fd30e9b5c11f586d5b0aeb55889ed2fac6215
trevthedev254 <trevetech@gmail.com>
2022-07-06 16:32:30 +0000

    set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     add = 0
-    for i in range(len(my_list)):
-        if my_list[i].count == 1:
-            add += i
+    for i in set(my_list):
+        add += i
     return add
